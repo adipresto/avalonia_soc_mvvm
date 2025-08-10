222309f34b15d141d5d8a70ea33a82097d7137ca
prasasdi <rizky.adie7@gmail.com>
2025-08-10 21:46:40 +0700

    update

--- MainAplikasi/ViewModels/MainWindowViewModel.cs
+++ MainAplikasi/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace MainAplikasi.ViewModels
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace MainAplikasi.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        public string Greeting { get; } = "Halo Dunia, aku AvaloniaUI!";
+
+        public ICommand cmd { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+            cmd = ReactiveCommand.Create(funccmd, this.WhenAnyValue(x => x.cancmd));
+        }
+
+        private bool cancmd { get; set; } = true;
+        private void funccmd()
+        {
+
+        }
     }
 }
